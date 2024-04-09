7d5a8e865dfb14cd5a2977df0864a2caab683393
Sofia Solis <sofis64@gmail.com>
2024-04-09 11:19:18 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Recipe from './components/Recipe'
+import Pagination from './components/Pagination'
 
-function App() {
+export default function App() {
+//Setting Recipes Variables
+const [recipes,setRecipes]= React.useState([])
+
+
+//Fetching Recipes
+function fetchRecipes(){
+fetch('https://dummyjson.com/recipes?limit=50')
+.then(res => res.json())
+.then(data=>{
+  if(data && data.recipes){
+    setRecipes(data.recipes)
+  }
+})
+}
+React.useEffect(()=>fetchRecipes(),[])
+
+
+//Setting Pages Variables
+const [page,setPage]= React.useState(1)
+
+
+
+
+// Setup the breakpoint variable
+let breakpoint;
+// Get the current breakpoint
+var getBreakpoint = function () {
+	return window.innerWidth;
+};
+
+// Calculate breakpoint on page load
+breakpoint = getBreakpoint();
+// Recalculate breakpoint on resize
+window.addEventListener('resize', function () {
+	breakpoint = getBreakpoint();
+  defineItems();
+}, false);
+
+var mobile = 576;
+var tablet = 992;
+let gridContainer= document.getElementsByClassName('cards-container')
+const defineItems=()=>{
+  if (breakpoint < mobile) {
+    gridContainer[0].style.gridTemplateColumns= " 1fr";
+  }
+  else if (breakpoint < tablet) {
+    gridContainer[0].style.gridTemplateColumns= " 1fr 1fr";
+  }
+  else {
+    gridContainer[0].style.gridTemplateColumns= " 1fr 1fr 1fr";
+  }
+  } 
+
+
+
+//Mapping Recipe Elements in pages
+const recipeElements=recipes.slice(page * 9 - 9, page * 9).map(element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Recipe 
+      key={element.id}
+      item={element}
+      />
+  )
+})
+
+//Function to select page
+const pageSelector= (selectedPage)=>{
+  if (selectedPage !== page){
+    setPage(selectedPage)
+  }
 }
 
-export default App;
+
+  return (
+    <div className="App" >
+      <div class='container bg-dark text-center'>
+        <h2 class='text-light pt-3 pb-3'> Recipes Album</h2>
+        <p class='text-light'>Fetching data from https://dummyjson.com/recipes</p>
+        <Pagination recipes={recipes}
+        pageSelector={pageSelector} page={page} />
+        <div className='cards-container'>
+          {recipeElements}
+        </div>
+        <Pagination recipes={recipes}
+        pageSelector={pageSelector} page={page} />
+      </div>
+    </div>
+  );
+
+
+  }
